26a7e45ba900dae388e8ee075653baa2650f8bf6
vian <vianv31253@gmail.com>
2024-10-12 22:32:30 +0700

    menambahkan sebuah input dengan decoration

--- lib/main.dart
+++ lib/main.dart
@@ -24,12 +24,37 @@ class HalamanUtama extends StatefulWidget {
 }
 
 class _HalamanUtamaState extends State<HalamanUtama> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController xHarga = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contoh Formulir'),
         backgroundColor: Colors.redAccent,
+      ),
+      body: Form(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: xHarga,
+                  decoration: InputDecoration(
+                      labelText: 'Harga Barang',
+                      hintText: 'Input Harga Barang',
+                      prefixIcon: Icon(Icons.monetization_on),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(20))),
+                  keyboardType: TextInputType.number,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
